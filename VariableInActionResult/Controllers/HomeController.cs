b50d8bbbc57a1fbe180bef8ef55ed0e016fed18e
saeed-balkani <saeedbalkani@gmail.com>
2020-10-10 18:02:04 +0200

    new commit

---
+++ VariableInActionResult/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VariableInActionResult.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index1(string Username, string Password)
+        {
+            ViewBag.message = Username + " " + " " + Password;
+            return View();
+        }
+    }
+}
